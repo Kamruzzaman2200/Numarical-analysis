c19c5cfe079b95c478f88a541843af3c7c9aa2c2
MD. Kamruzzaman <103466655+Kamruzzaman2200@users.noreply.github.com>
2024-02-11 22:20:44 +0600

    Create lab_2_rapson.py

---
+++ lab_2_rapson.py
@@ -0,0 +1,57 @@
+f = [1,-2,0,4]
+df = [3,-4,0]
+def func(f, x):
+    sum=0
+    temp=0
+    for i in range(len(f)):
+        sum+=f[i]*(x**(len(f)-1-temp))
+        temp+=1
+
+    return sum
+def newton_raphson(f, df, x0, tol):
+    # f = the list which holds the coefficients of the equation
+    # df =  the list which holds the coefficients of the derived equation
+    # x0 = starting point
+    # tol = tolerace level
+    x_old= x0
+
+    while True:
+        f_x_old=func(f,x_old)
+        df_x_old= func(df,x_old)
+
+      # Use the equation x_new = x_old - f(x) / df(x), to get the updated value
+        x_new =x_old-f_x_old /df_x_old
+      # Check the tolerance level with the equation relative_error = abs((x_old - x_new) / x_new) * 100
+        relative_error = abs((x_new - x_old) / x_new) * 100
+      # If the relative error is below tolerance level, return the x value.
+        if relative_error < tol:
+            return x_new
+      # Update the x_old value
+        x_old = x_new
+# Use the equation with the tolerance level of 0.0001
+result = newton_raphson(f, df, 5, 0.00001)
+# check if we get f(x) = 0
+func(f, result)
+def secant(f, x_current, x_prev,  tol):
+  # f = the list which holds the coefficients of the equation
+  # x_current = 1st intial point
+  # x_prev = 2nd intial point
+  # tol = tolerace level
+
+  while(True):
+     f_x_current = func(f, x_current)
+     f_x_prev = func(f, x_prev)
+    # Use the equation x_next = x_current - (f(x_current) * (x_current - x_prev)) / (f(x_current) - f(x_prev)), to get the updated value
+     x_next = x_current - (f_x_current * (x_current - x_prev)) / (f_x_current - f_x_prev)
+    # Check the tolerance level with the equation relative_error = abs((x_next - x_current) / x_next) * 100
+     relative_error = abs((x_next - x_current) / x_next) * 100
+    # If the relative error is below tolerance level, return the x_next value.
+     if relative_error < tol:
+      return x_next
+    # Update the x_prev and x_current value
+     x_prev = x_current
+     x_current = x_next
+# Use the equation with the tolerance level of 0.0001
+result = secant(f, 5, 7,  0.00001)
+# check if we get f(x) = 0
+print(func(f,result))
